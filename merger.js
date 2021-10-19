1f02e58c631cf050a38af9ec6bee90587778eee2
Serghei Piteli <Serghei.Piteli@morningstar.com>
2021-10-18 20:39:49 -0500

    ready

---
+++ merger.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+let object1, object2 = {};
+
+object1 = JSON.parse(fs.readFileSync('./file1.json'));
+object2 = JSON.parse(fs.readFileSync('./file2.json'));
+
+var merged = {};
+Object.keys(object1).forEach(k => merged[k] = object1[k]);
+Object.keys(object2).forEach(k => { 
+   if (merged[k] !== object2[k]) {
+       k = Object.assign(merged[k], object2[k])
+   }
+});
+
+let data = JSON.stringify(merged);
+fs.writeFileSync('merged.json', data);
+
+
