ffd861bf08154caab4364f4de7ce8480c1858ffd
Serghei Piteli <Serghei.Piteli@morningstar.com>
2021-10-18 20:46:30 -0500

    replace var with let

--- merger.js
+++ merger.js
@@ -5,7 +5,7 @@ let object1, object2 = {};
 object1 = JSON.parse(fs.readFileSync('./file1.json'));
 object2 = JSON.parse(fs.readFileSync('./file2.json'));
 
-var merged = {};
+let merged = {};
 Object.keys(object1).forEach(k => merged[k] = object1[k]);
 Object.keys(object2).forEach(k => { 
    if (merged[k] !== object2[k]) {
